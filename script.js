ca2ca7a9fdf390f9a0d1447a22f0eafc53509f1a
KasperTonsgaardMerkle <kasper.tonsgaard@merkle.com>
2024-11-21 17:57:25 +0100

    add useless form stuff

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const currentPage = window.location.pathname.split('/').pop();
 
 const activeLink = document.querySelector('a[href="' + currentPage + '"]');
 activeLink.style.background = '#2646a6';
+
+
+function contactSubmit() {
+    alert("Thank you for your message! We will get back to you soon.");
+}
+
+function indexSubmit() {
+    alert("this was a useless form, but thank you very much anyway");
+}
